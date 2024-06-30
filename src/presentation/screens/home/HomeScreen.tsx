4f456b5aadb2ea3788bd9bc7fba3dda97c6cd07a
Juanpablo9717 <juanpablo9717@gmail.com>
2024-06-30 18:22:36 -0500

    Initial Project - initial files created - react navigation installed - bottom tan navigator installed and setup

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../../theme/app.theme';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../../theme/app.theme';
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
